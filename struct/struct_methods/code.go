f1651cb2eff43a46ca41e5abee1651e6b9d986cd
ThapaSurace <zorosenpai456@gmail.com>
2024-06-24 08:13:11 +0545

    struct method

---
+++ struct/struct_methods/code.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type authenticationInfo struct {
+	username string
+	password string
+}
+
+func (auth authenticationInfo) getBasicAuth() string {
+	return fmt.Sprintf("username:%s,password:%s", auth.username, auth.password)
+}
+
+// don't touch below this line
+
+func test(authInfo authenticationInfo) {
+	fmt.Println(authInfo.getBasicAuth())
+	fmt.Println("====================================")
+}
+
+func main() {
+	test(authenticationInfo{
+		username: "Google",
+		password: "12345",
+	})
+	test(authenticationInfo{
+		username: "Bing",
+		password: "98765",
+	})
+	test(authenticationInfo{
+		username: "DDG",
+		password: "76921",
+	})
+}
